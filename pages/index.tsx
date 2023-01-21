d54a3049be586df6db663c2fd203dd281df16235
deniskurochkin <deniskurochkin@ozon.ru>
2023-01-21 12:51:24 +0400

    feat: install typescript and stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from '~/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +18,7 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
             <a
